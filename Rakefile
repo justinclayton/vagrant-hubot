d4eac739fcf1369d8c46e906ea1eaa0e35b6882e
Justin Clayton <justin@claytons.net>
2013-08-26 02:24:55 -0700

    rearranged ALL the deck chairs; trying to determine name of the ship im currently on...

---
+++ Rakefile
@@ -0,0 +1,7 @@
+# Rakefile
+
+desc "ssh in and get hubot dev prompt"
+task :default do
+  # noop
+end
+
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,25 +1,8 @@
-node /hubot/ {
+node default {
   include stdlib
-  include vagrant
   class { 'apt': stage => 'setup' }
 
-  class { 'irc':
-    network_name  => 'irc.hubot_dev.local',
-    network_desc  => 'hubot dev server',
-    admin_name    => 'Your Mom',
-    admin_email   => 'happiness@betweenherlegs.com',
-    operator_name => 'admin',
-    operator_pass => 'hubot_dev',
-    listen_ip     => $::ipaddress,
-  }
-
-  class { 'nodejs_squeeze': }
-  -> class { 'git': }
-  -> class { 'hubot':
-      adapter       => 'irc',
-      irc_nickname  => 'crunchy',
-      irc_server    => $::ipaddress,
-      irc_rooms     => ['#soggies'],
-      vagrant_hubot => true,
-   }
+  class { 'nodejs_squeeze': } ->
+  class { 'git': } ->
+  class { 'hubot': }
 }
